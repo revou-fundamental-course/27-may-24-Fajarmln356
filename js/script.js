115df02a4365a94285dc252f3e5b4a8c2e727772
Fajar Maulana <fajarmln356@gmail.com>
2024-06-05 11:38:24 +0700

    respnsive website 400px commit

--- js/script.js
+++ js/script.js
@@ -7,22 +7,3 @@
 
 //replaceName ()
 
-document.getElementById('messageForm').addEventListener('submit', function(event) {
-  event.preventDefault(); // Prevent the form from submitting the traditional way
-
-  // Get current time
-  let currentTime = new Date().tolocaleString();
-
-  // Get form values
-  let name = document.getElementById('name').value;
-  let dob = document.getElementById('birthDay').value;
-  //let gender = document.querySelector('input[name="gender"]:checked').value;
-  let message = document.getElementById('message').value;
-
-  // Display values
-  document.getElementById('currentTime').textContent = currentTime;
-  document.getElementById('outputName').textContent = name;
-  document.getElementById('outputDob').textContent = dob;
-  //document.getElementById('outputGender').textContent = gender;
-  document.getElementById('outputMessage').textContent = message;
-});
