490882d9b2b79e1336d2a096c2b774267ec2f483
Fajar Maulana <fajarmln356@gmail.com>
2024-06-06 10:46:31 +0700

    validate form akhirnya selesai jugaaaaaa dan reset tampilan message

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 // Javascript Page //
 
-//function replaceName() {
- //   let name = prompt("What is your name?", "");
-   // document.getElementById("name").innerHTML = name + "!"
-// }
 
-//replaceName ()
+function replaceName() {
+ let name = prompt("What is your name?", "");
+  document.getElementById("name").innerHTML = name + "!"
+}
+
+replaceName ()
+
+
 function showSidebar (){
   const sidebar = document.querySelector('.sidebar');
   sidebar.style.display = 'flex';
@@ -16,14 +19,15 @@ function hideSidebar () {
   sidebar.style.display = 'none';
 }
 
-function validateForm() {
-  const name = document.forms["message-Form"]["name"].value;
-  const birthday = document.forms["message-Form"]["birthDay"].value;
-  const gender = document.forms["message-Form"]["gender"].value;
-  const message = document.forms["message-Form"]["message"].value;
 
-  if (name == '' || birthday == '' || gender == '' || message == ''){
-      document.getElementById("error-message").innerHTML = 'Message cannot be empty';
+function displayResult() {
+
+  const name = document.forms["message-Form"]["name"].value;
+  const birthday = document.forms["message-Form"]["birthday"].value;
+  const gender = document.forms["message-Form"]["gender"].value;
+  const message = document.forms["message-Form"]["comment"].value;
+
+  if (name == '' || birthday == '' || gender == '' || message == '') {
       return false;
   }
 
@@ -31,7 +35,6 @@ function validateForm() {
   const date = currentTime.toLocaleDateString();
   const time = currentTime.toLocaleTimeString();
 
-  document.getElementById("error-message").innerHTML = '';
   setSenderform(name, birthday, gender, message, date, time);
 
   return false;
@@ -39,7 +42,7 @@ function validateForm() {
 }
 
 function setSenderform(name, birthday, gender, message, date, time ){
-    document.getElementById("current-date").innerHTML = date;
+    document.getElementById("current-date").innerHTML = date + "-";
     document.getElementById("current-time").innerHTML = time;
     document.getElementById("output-name").innerHTML = name;
     document.getElementById("output-birthday").innerHTML = birthday;
